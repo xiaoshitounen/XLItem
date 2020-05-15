181f29c8e0caeac46807be47dffc99dad598aee1
xulei <2867584387@qq.com>
2020-05-15 14:25:00 +0800

    测试4

--- app/src/main/java/swu/xl/item/MainActivity.java
+++ app/src/main/java/swu/xl/item/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,9 +14,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        XLItem item = new XLItem(this, R.drawable.ic_launcher_background, "测试", 0, Color.BLACK, Color.MAGENTA, R.layout.item_layout);
+        final XLItem item = new XLItem(this, R.drawable.ic_launcher_background, "测试", 0, Color.BLACK, Color.MAGENTA, R.layout.item_layout);
 
         RelativeLayout layout = findViewById(R.id.root);
         layout.addView(item);
+
+        final int[] flag = {0};
+        item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (flag[0] == 0){
+                    item.changeStatus(XLItem.STATUS_SELECT);
+
+                    flag[0] = 1;
+                }else {
+                    item.changeStatus(XLItem.STATUS_NORMAL);
+
+                    flag[0] = 0;
+                }
+            }
+        });
     }
 }
